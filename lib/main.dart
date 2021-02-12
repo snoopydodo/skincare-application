a5a5c4b2034d83f9a69cec76a18e9dd33a54f479
snoopydodo <nuttida.nap@student.mahidol.edu>
2021-02-13 02:23:39 +0700

    Added all new code

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,142 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+    void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Skincare Application 2',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+    class MyApp extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'ListViews',
+          theme: ThemeData(
+            primarySwatch: Colors.teal,
+            scaffoldBackgroundColor: Color(0xFFE0F2F1),
+          ),
+          home: Scaffold(
+            appBar: AppBar(title: Text('La Roche-Posay Shop')),
+            body: BodyLayout(),
+          ),
+        );
+      }
+    }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+    class BodyLayout extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return _myListView(context);
+      }
+    }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    Widget _myListView(BuildContext context) {
+      return ListView(
+        children: ListTile.divideTiles(
+          context: context,
+          tiles: [
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/product/1.png'),
+              ),
+              title: Text('EFFACLAR GEL MOUSS T50ML F GB'),
+              subtitle: Text('Price: ฿290'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/product/2.png'),
+              ),
+              title: Text('EFFACLAR DUO 40ML'),
+              subtitle: Text('Price: ฿990'),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/product/3.png'),
+              ),
+              title: Text('EFFACLAR DUO(+) SPF30 40ML'),
+              subtitle: Text('Price: ฿1,190'),
+            ),
+             ListTile(
+               leading: CircleAvatar(
+                 backgroundImage: AssetImage('assets/product/4.png'),
+               ),
+              title: Text('Anthelios Drytouch Cream SPF50+ 50ML'),
+              subtitle: Text('Price: ฿1,300'),
+            ),
+             ListTile(
+               leading: CircleAvatar(
+                 backgroundImage: AssetImage('assets/product/5.png'),
+               ),
+              title: Text('Anthelios Shaka Fluid SPF50+ 50ML'),
+              subtitle: Text('Price: ฿1,400'),
+            ),
+             ListTile(
+               leading: CircleAvatar(
+                 backgroundImage: AssetImage('assets/product/6.png'),
+               ),
+              title: Text('Anthelios Ultra Comfort SPF50+ 50ML'),
+              subtitle: Text('Price: ฿1,300'),
+            ),
+             ListTile(
+               leading: CircleAvatar(
+                 backgroundImage: AssetImage('assets/product/7.png'),
+               ),
+              title: Text('Anthelios XI SPF 50+ Lotion 100ML'),
+              subtitle: Text('Price: ฿1,100'),
+            ),
+             ListTile(
+               leading: CircleAvatar(
+                 backgroundImage: AssetImage('assets/product/8.png'),
+               ),
+              title: Text('EFL MICELLAR WATER OILY SKIN 100ML'),
+              subtitle: Text('Price: ฿400'),
+            ),
+             ListTile(
+               leading: CircleAvatar(
+                 backgroundImage: AssetImage('assets/product/9.png'),
+               ),
+              title: Text('EFFACLAR DUO(+) 7.5ML'),
+              subtitle: Text('Price: ฿199'),
+            ),
+             ListTile(
+               leading: CircleAvatar(
+                 backgroundImage: AssetImage('assets/product/10.png'),
+               ),
+              title: Text('THERMAL SPRING WATER 100ML'),
+              subtitle: Text("Price: ฿310"),
+            ),
+             ListTile(
+               leading: CircleAvatar(
+                 backgroundImage: AssetImage('assets/product/11.png'),
+               ),
+              title: Text('Invisible Spray SPF 50+ 200ML'),
+              subtitle: Text("Price: ฿1,300"),
+            ),
+             ListTile(
+               leading: CircleAvatar(
+                 backgroundImage: AssetImage('assets/product/12.png'),
+               ),
+              title: Text('REDERMIC 40ML'),
+              subtitle: Text('Price: ฿2,100'),
+            ),
+             ListTile(
+               leading: CircleAvatar(
+                 backgroundImage: AssetImage('assets/product/13.png'),
+               ),
+              title: Text('EFFACLAR PURIFYING GEL 200ML'),
+              subtitle: Text('Price: ฿890'),
+            ), ListTile(
+               leading: CircleAvatar(
+                 backgroundImage: AssetImage('assets/product/14.png'),
+               ),
+              title: Text('EFFACLAR PURIFYING GEL 400ML'),
+              subtitle: Text('Price: ฿1,200'),
+            ),
+             ListTile(
+               leading: CircleAvatar(
+                 backgroundImage: AssetImage('assets/product/15.png'),
+               ),
+              title: Text('EFFACLAR SERUM 30ML'),
+              subtitle: Text('Price: ฿1,300'),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+        ).toList(),
+      );
+    }
